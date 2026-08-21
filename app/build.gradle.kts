47eb20765af4a35fecb72937434aa19fe8e52a4f
SUFelix <felixsummerer@freenet.de>
2026-08-21 14:08:14 +0200

    target sdk ->36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.felix.mealplanner20"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.felix.mealplanner20"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 13
         versionName = "1.8.7"
 
@@ -35,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,6 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.6.0") // Use the appropriate version
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0") // Use the appropriate version
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48") // Use the latest version
     }
 }
